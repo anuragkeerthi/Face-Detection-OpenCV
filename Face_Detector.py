714d355d453f8600a5b1558ba6660e3a3851cb97
Anurag <anurag@pop-os.localdomain>
2020-09-25 23:40:07 +0530

    Converting the image to GrayScale

--- Face_Detector.py
+++ Face_Detector.py
@@ -5,5 +5,10 @@ trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 img = cv2.imread('RDJ.png')
 
-cv2.imshow('Picture', img)
-cv2.waitKey()
+#Convert to Grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#cv2.imshow('Picture', grayscaled_img)
+#cv2.waitKey()
+
+
