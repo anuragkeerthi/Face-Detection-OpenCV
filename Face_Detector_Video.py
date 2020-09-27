29d48c2d9f35dfa8ada84b60996186ff0bf327cc
Anurag <anurag@pop-os.localdomain>
2020-09-27 22:29:13 +0530

    Done with the Face Detector Video

--- Face_Detector_Video.py
+++ Face_Detector_Video.py
@@ -4,8 +4,8 @@ from random import randrange
 #imported trained data from https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#Use '0' to use your default webcam
-webcam = cv2.VideoCapture(0)
+#Use VideoCapture(0) to use your default webcam
+webcam = cv2.VideoCapture(2)
 
 # To iterate all the frames 
 while True:
@@ -14,10 +14,16 @@ while True:
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
     for (x, y, w, h) in face_coordinates:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)), 4)
 
     cv2.imshow('Webcam', frame)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    # Ascii value of Q=81 and q=113
+    if key == 81 or key == 113:
+        break
+    #webcam.release()
+
+
 
 
 
