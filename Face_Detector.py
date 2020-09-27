b464664164186a52d44de16231739a7210f77268
Anurag <anurag@pop-os.localdomain>
2020-09-27 20:22:12 +0530

    Drawing Rectangle over the face

--- Face_Detector.py
+++ Face_Detector.py
@@ -8,7 +8,15 @@ img = cv2.imread('RDJ.png')
 #Convert to Grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#cv2.imshow('Picture', grayscaled_img)
-#cv2.waitKey()
+#Detecting Faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+#print(face_coordinates)  [[ 55  49 115 115]]
+ 
+(x, y, w, h) = face_coordinates
+cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
+
+cv2.imshow('Picture', img)
+cv2.waitKey()
 
 
