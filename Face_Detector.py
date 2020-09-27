2a83773ae6be9bd50e2e45204441d3575660f7d6
Anurag <anurag@pop-os.localdomain>
2020-09-27 21:40:57 +0530

    Done with the Image Face Detection

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,4 +1,5 @@
 import cv2 
+from random import randrange
 
 #imported from https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -11,11 +12,12 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #Detecting Faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-#print(face_coordinates)  [[ 55  49 115 115]]
+#print(face_coordinates)
  
-(x, y, w, h) = face_coordinates[0]
-cv2.rectangle(img, (x, y), (x+w, y+h), (0,255,0), 2)
+for (x, y, w, h) in face_coordinates:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)), 2)
 
+#Display the image
 cv2.imshow('Picture', img)
 cv2.waitKey()
 
